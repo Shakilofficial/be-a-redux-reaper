d0357865392039f63b3310d0cc97b20f519be55a
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2025-01-03 00:31:51 +0600

    complete counter app with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { Button } from "./components/ui/button";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state) => state.counter.count);
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-4xl font-bold mb-10">Counter App with Redux</h1>
-        <Button>Increment</Button>
-        <div className="my-4">0</div>
-        <Button>Decrement</Button>
+        <Button onClick={handleIncrement}>Increment</Button>
+        <div className="my-4 text-4xl font-semibold">{count}</div>
+        <Button onClick={handleDecrement}>Decrement</Button>
       </div>
     </>
   );
---
+++ src/redux/features/counter/counterSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.count++;
+    },
+    decrement: (state) => {
+      state.count--;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
